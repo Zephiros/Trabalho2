5dcb8e6b4bb375255babdd7b3af0b2003944e97e
Zephiros <alexzephiros@hotmail.com>
2014-05-29 15:53:06 -0300

    NoTerceiroSempreFunciona

--- src/br/univali/poo/empresa/ContraCheque.java
+++ src/br/univali/poo/empresa/ContraCheque.java
@@ -1,9 +1,40 @@
-package br.univali.poo.empresa;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trabalhom2.empresa;
 
 /**
- *
- * @author Alex
+ * Referente ao contracheque
+ * @author Heverton de Lemos - Alex Batista
  */
-public class ContraCheque {
+public class ContraCheque
+{
+    private String relatorio;
     
+    /**
+     *  Retorna um contracheque.
+     * @return o contra cheque no formato:
+     * "departamento - nome - dataAdmissao - salarioBase - horaReferencia - 
+     * horaTrabalhada - salarioLiquido - INSS - impostoDeRenda - anuenio - 
+     * salarioFinal"
+     */
+    public String getRelatorio()
+    {
+        return this.relatorio;
+    }
+    
+    /**
+     * Insere dados no contracheque
+     * @param relatorio string com os dados no formato:
+     * "departamento - nome - dataAdmissao - salarioBase - horaReferencia - 
+     * horaTrabalhada - salarioLiquido - INSS - impostoDeRenda - anuenio - 
+     * salarioFinal"
+     */
+    public void setRelatorio(String relatorio)
+    {
+        this.relatorio = relatorio; 
+    }
 }
