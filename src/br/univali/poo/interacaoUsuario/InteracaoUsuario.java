994cc8e96993e6a868b23617c85ef51fea0e4b9c
Heverton de Lemos <v3rton@hotmail.com>
2014-06-01 16:48:42 -0300

    interaçãousuario

--- src/br/univali/poo/interacaoUsuario/InteracaoUsuario.java
+++ src/br/univali/poo/interacaoUsuario/InteracaoUsuario.java
@@ -15,7 +15,7 @@ import br.univali.poo.empresa.Regular;
 import java.util.List;
 
 /**
- *  Classe que faz a interação do usuário com as classes de processamento e de entidade.
+ * Classe que faz a interação do usuário com as classes de processamento e de entidade.
  * @author Heverton de Lemos - Alex Batista
  */
 public class InteracaoUsuario {
@@ -24,46 +24,11 @@ public class InteracaoUsuario {
     List<Funcionario> funcionario;
     
     /**
-     * Cria uma nova empresa, com nome e uma folha de pagamento zerada.
-     * @param nome nome da empresa.
+     * Verifica tipo de funcionario.
+     * @param funcionario funcionario a ser verificado.
+     * @return retorna [1]para Regular, [2]para Diretor, [3]para Externo
+     * e [0] caso não for nenhum desses.
      */
-    public void criaEmpresa(String nome)
-    {
-        this.empresa = new Empresa(nome);
-    }
-    
-    //Permitir a inclusão de funcionários.
-    public void incluirFuncionario(int tipo, float salarioL)
-    {
-        folhaPagamento = empresa.getUltimaFolhaPagamento();
-        folhaPagamento.cadastraFuncionario(tipo, salarioL);
-    }
-    
-    //Permitir a impressão da folha de pagamento informando o nome dos funcionários e o seus respectivos salários.
-    public void imprimiFolhaPagamento()
-    {
-        folhaPagamento = empresa.getUltimaFolhaPagamento();
-        funcionario = folhaPagamento.getFuncionarios();
-        
-        for(int i = 0; i <= folhaPagamento.getQuantidadeFuncionario(); i++)
-        {
-            int tipo = tipoFuncionario(funcionario.get(i));
-            
-            switch(tipo)
-            {
-                case 1: System.out.println(((Regular) funcionario.get(i)).getNome());
-                        System.out.println(((Regular) funcionario.get(i)).getSalarioBase());
-                    break;
-                case 2: System.out.println(((Diretor) funcionario.get(i)).getNome());
-                        System.out.println(((Diretor) funcionario.get(i)).getSalarioBase());
-                    break;
-                case 3: System.out.println(((Externo) funcionario.get(i)).getNome());
-                        System.out.println(((Externo) funcionario.get(i)).getsalarioLiquido());
-                    break;
-            }
-        }
-    }
-    
     public int tipoFuncionario(Funcionario funcionario)
     {
         if(funcionario instanceof Regular)
@@ -77,9 +42,58 @@ public class InteracaoUsuario {
         else
                 return 0;
     }
+    
+    /**
+     * Cria uma nova empresa, com nome e uma folha de pagamento zerada.
+     * @param nome nome da empresa.
+     */
+    public void criaEmpresa(String nome)
+    {
+        this.empresa = new Empresa(nome);
+    }
+    
+    //Permitir a inclusão de funcionários.
+    public void incluirFuncionario(int tipo, float salarioL)
+    {
+        folhaPagamento = empresa.getFolhaPagamento();
+        folhaPagamento.cadastraFuncionario(tipo, salarioL);
+    }
+    
+    //Permitir a impressão da folha de pagamento informando o nome dos funcionários e o seus respectivos salários.
+    public void imprimiFolhaPagamento()
+    {
+        folhaPagamento = empresa.getFolhaPagamento();
+        funcionario = folhaPagamento.getFuncionarios();
+        
+        for(int i = 0; i <= folhaPagamento.getQuantidadeFuncionario(); i++)
+        {
+            int tipo = tipoFuncionario(funcionario.get(i));
+            
+            switch(tipo)
+            {
+                case 1: System.out.println("Nome: "+((Regular) funcionario.get(i)).getNome());
+                        System.out.println("Salario: "+((Regular) funcionario.get(i)).calculaSalarioLiquidoComDesconto());
+                    break;
+                case 2: System.out.println("Nome: "+((Diretor) funcionario.get(i)).getNome());
+                        System.out.println("Salario: "+((Diretor) funcionario.get(i)).calculaSalarioLiquidoComDesconto());
+                    break;
+                case 3: System.out.println("Nome: "+((Externo) funcionario.get(i)).getNome());
+                        System.out.println("Salario: "+((Externo) funcionario.get(i)).calculaSalarioLiquidoComDesconto());
+                    break;
+            }
+        }
+    }
+    
     //Permitir a impressão da folha de pagamento informando o nome dos funcionários e o seus respectivos salários (em ordem crescente de salário).
+    
     //Permitir a visualização do valor total da folha de pagamento.
+    
     //Permitir a consulta do funcionário (independente do tipo de funcionário) que recebe o maior salário.
+    public void maiorSalario()
+    {
+        
+    }
+    
     //Permitir a consulta de todos os funcionários alocados em um determinado departamento da empresa.
 
 }
